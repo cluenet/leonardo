4ea16657a17ee5fa56ac1a48febe1990340bdf4a
Henrik Hodne <me@henrikhodne.com>
2012-11-05 09:16:43 +0100

    Improve classifier-test macro

--- test/leonardo/message_classifier_test.clj
+++ test/leonardo/message_classifier_test.clj
@@ -3,12 +3,15 @@
         [leonardo.message-classifier]))
 
 (defmacro classifier-test
-  [name classifier sentence]
-  `(deftest ~name
+  [classifier sentence]
+  `(deftest ~(symbol (format "%s-test" (name classifier)))
      (let [sentence# ~sentence
            classifiers# (classify-sentence sentence#)]
        (is (contains? classifiers# ~classifier)))))
 
-(classifier-test clueful-sentence-test :clueful "This is a clueful sentence.")
-(classifier-test normal-sentence-test :normal "This is a normal sentence")
-(classifier-test abnormal-sentence-test :abnormal "this is an abnormal sentence")
+(classifier-test :clueful
+                 "This is a clueful sentence.")
+(classifier-test :normal
+                 "This is a normal sentence")
+(classifier-test :abnormal
+                 "this is an abnormal sentence")
